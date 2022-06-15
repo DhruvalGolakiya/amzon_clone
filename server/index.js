83d1301e7290fc6cf4b13e19172183935917b3f5
dhruval <dhruval257@gmail.com>
2022-06-15 23:55:47 +0530

    server commit

---
+++ server/index.js
@@ -0,0 +1,12 @@
+ console.log("Helllo There");
+
+const express = require('express');
+
+const PORT = 3000;
+
+const app = express();
+// Creating API
+
+app.listen(PORT,"0.0.0.0",()=>{
+    console.log(`Connected at ${PORT}`);
+});
