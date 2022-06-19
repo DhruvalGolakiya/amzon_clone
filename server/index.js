15f285b716079d0e095fa8388b31c1d5b0b246c8
dhruval <dhruval257@gmail.com>
2022-06-19 17:05:03 +0530

    server2 commit

--- server/index.js
+++ server/index.js
@@ -1,10 +1,24 @@
- console.log("Helllo There");
-
 const express = require('express');
-
+const mongoose = require('mongoose');
 const PORT = 3000;
 
+const authRouter = require('./routes/auth');
 const app = express();
+const DB = "mongodb+srv://Dhruval:dhruvalMDDK257@cluster0.dewuzkv.mongodb.net/?retryWrites=true&w=majority";
+
+
+//middleware
+app.use(express.json())
+app.use(authRouter);
+
+
+//connection
+mongoose.connect(DB).then(()=>{
+    console.log('Successfull');
+})
+   .catch((e)=>{
+    console.log(e);
+   })
 // Creating API
 
 app.listen(PORT,"0.0.0.0",()=>{
---
+++ server/routes/auth.js
@@ -0,0 +1,33 @@
+const express = require('express');
+const User = require('../models/user');
+const bcryptjs = require('bcryptjs');
+const authRouter = express.Router();
+
+authRouter.post('/api/signup', async (req,res) => { 
+  try{
+    
+    const {name,email,password} = req.body;
+    
+    const existUser = await User.findOne({email});
+    if(existUser) {
+        return res.status(400).json({msg : "User with Same Email Alreay exist!!"})
+    }
+
+    const hashedPasword = await bcryptjs.hash(password,8);
+
+    let user = new User({
+        email,
+        password: hashedPasword,
+        name, 
+    })
+    user =  await user.save();
+    res.json(user);
+  }
+  catch(e){
+    res.status(500).json({error : e.message});
+  }
+     // post data into Database
+     // return data to user
+});
+
+module.exports = authRouter;
